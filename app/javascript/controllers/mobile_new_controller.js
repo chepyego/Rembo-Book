c2938f1328e80b478f36456a247350659aa1fc87
chepyego <jchepyego@gmail.com>
2026-06-19 00:15:57 +0300

    edit mobile navigation

--- app/javascript/controllers/mobile_new_controller.js
+++ app/javascript/controllers/mobile_new_controller.js
@@ -2,8 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["menu2"]
+
+   
   
   toggle(){
     this.menu2Target.classList.toggle("hidden")
   }
+
 }
